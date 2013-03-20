576dce5d2c60522b21beef51630264a1a3f675ac
masterchief117 <bstewart@catalystitservices.com>
2013-03-20 09:25:36 -0700

    Added some comments to DbSetWrapper.cs and IPracticeGDVPDao.cs.

--- CardShop/CardShop/Daos/DbSetWrapper.cs
+++ CardShop/CardShop/Daos/DbSetWrapper.cs
@@ -7,50 +7,97 @@ using System.Data.Entity.Infrastructure;
 
 namespace CardShop.Daos
 {
+    /// <summary>
+    /// A wrapper for DbSet.
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
     public class DbSetWrapper<TEntity> : IDbSet<TEntity> where TEntity : class
     {
         private DbSet<TEntity> db {get;set;}
-
+        /// <summary>
+        /// Constructor, takes in Entity. Sets entity for wrapper class
+        /// </summary>
+        /// <param name="db"> Entity found in PracticeGDVPEntities.
+        ///  If confused, look in file. Use field names found there.</param>
         public DbSetWrapper(DbSet<TEntity> db)
         {
             this.db = db;
         }
-
+        /// <summary>
+        /// Wrapper for Add in DbSet
+        /// </summary>
+        /// <param name="entity">Entity found in PracticeGDVPEntities.
+        ///  If confused, look in file. Use field names found there.</param>
+        /// <returns></returns>
         public TEntity Add(TEntity entity)
         {
             return db.Add(entity);
         }
-
+        /// <summary>
+        /// Attaches an Entity to the context.
+        /// </summary>
+        /// <param name="entity">Entity found in PracticeGDVPEntities.
+        ///  If confused, look in file. Use field names found there.</param>
+        /// <returns></returns>
         public TEntity Attach(TEntity entity)
         {
             return db.Attach(entity);
         }
-
+        /// <summary>
+        /// Creates an instance of the derived entity. Does not Add!!
+        /// </summary>
+        /// <typeparam name="TDerivedEntity">Entity found in PracticeGDVPEntities.
+        ///  If confused, look in file. Use field names found there.</typeparam>
+        /// <returns></returns>
         public TDerivedEntity Create<TDerivedEntity>() where TDerivedEntity : class, TEntity
         {
             return db.Create<TDerivedEntity>();
         }
-
+        /// <summary>
+        /// Creates an instance of the derived entity. Does not Add!!
+        /// </summary>
+        /// <returns></returns>
         public TEntity Create()
         {
             return db.Create();
         }
-
+        /// <summary>
+        /// Find an entity with the given value at the given location.
+        /// Example: id = 17. set a new User,  User.Id = 17.  Find(User.Id) will find
+        /// where User.Id = 17
+        /// </summary>
+        /// <param name="keyValues">The values of the primary key for the entity to be found.
+        /// </param>
+        /// <returns></returns>
         public TEntity Find(params object[] keyValues)
         {
             return db.Find(keyValues);
         }
-
+        /// <summary>
+        /// Marks entity as ready to delete. must call context.SaveChanges
+        /// to delete.
+        /// </summary>
+        /// <param name="entity">Entity found in PracticeGDVPEntities.
+        ///  If confused, look in file. Use field names found there.</param>
+        /// <returns></returns>
         public TEntity Remove(TEntity entity)
         {
             return db.Remove(entity);
         }
-
+        /// <summary>
+        /// Changes DbSet to List.
+        /// </summary>
+        /// <returns></returns>
         public List<TEntity> ToList()
         {
             return db.ToList();
         }
-
+        /// <summary>
+        /// Executes a query.
+        /// </summary>
+        /// <param name="sql">SQL String </param>
+        /// <param name="parameters"> parameters for SQL string</param>
+        /// <returns></returns>
         public DbSqlQuery<TEntity> SqlQuery(string sql, params object[] parameters)
         {
             return db.SqlQuery(sql, parameters);
--- CardShop/CardShop/Daos/IPracticeGDVPDao.cs
+++ CardShop/CardShop/Daos/IPracticeGDVPDao.cs
@@ -12,13 +12,45 @@ namespace CardShop.Daos
     /// </summary>
     public interface IPracticeGDVPDao : IDisposable
     {
+        /// <summary>
+        /// DbSet for User
+        /// </summary>
+        /// <returns></returns>
         IDbSet<User> Users();
+        /// <summary>
+        /// DbSet for BaseballCard
+        /// </summary>
+        /// <returns></returns>
         IDbSet<BaseballCard> BaseballCards();
+        /// <summary>
+        /// DbSet for BaseballCardTransaction
+        /// </summary>
+        /// <returns></returns>
         IDbSet<BaseballCardTransaction> BaseballCardTransactions();
+        /// <summary>
+        /// DbSet for RuleSet
+        /// </summary>
+        /// <returns></returns>
         IDbSet<RuleSet> RuleSets();
+        /// <summary>
+        /// DbSet for Store
+        /// </summary>
+        /// <returns></returns>
         IDbSet<Store> Stores();
+        /// <summary>
+        /// DbSet for StoreInvetory
+        /// </summary>
+        /// <returns></returns>
         IDbSet<StoreInventory> StoreInventories();
+        /// <summary>
+        /// DbSet for Transaction
+        /// </summary>
+        /// <returns></returns>
         IDbSet<Transaction> Transactions();
+        /// <summary>
+        /// DbSet UserDiscount
+        /// </summary>
+        /// <returns></returns>
         IDbSet<UserDiscount> UserDiscounts();
     }
 }
