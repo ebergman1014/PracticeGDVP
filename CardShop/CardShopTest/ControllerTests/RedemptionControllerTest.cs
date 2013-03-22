e66af8275420f5f3b2696c594c6f4b02eab0f1dd
masterchief117 <brent@catalystitservices.com>
2013-03-22 13:53:36 -0700

    The name of a method test in RedemptionControllerTest.cs was confusing. Changed for clarity.

--- CardShop/CardShopTest/ControllerTests/RedemptionControllerTest.cs
+++ CardShop/CardShopTest/ControllerTests/RedemptionControllerTest.cs
@@ -7,7 +7,7 @@ namespace CardShopTest.ControllerTests
     public class RedemptionControllerTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ReedemTest()
         {
         }
     }
