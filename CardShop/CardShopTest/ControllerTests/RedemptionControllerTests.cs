6fd6a5f890d6dda514b0ced4c1ef2a16640f4d41
CommanderPaul <gaussandgo@gmail.com>
2013-03-22 15:00:28 -0700

    Working on RedemptionControllerTests.cs

--- CardShop/CardShopTest/ControllerTests/RedemptionControllerTests.cs
+++ CardShop/CardShopTest/ControllerTests/RedemptionControllerTests.cs
@@ -3,13 +3,30 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CardShopTest.ControllerTests
 {
+    /// <summary>
+    /// Test Class for RedemptionController
+    /// </summary>
+    /// 
     [TestClass]
     public class RedemptionControllerTests
     {
-        [TestMethod]
-        public void ReedemTest()
+        //  fields
+
+
+        /// <summary>
+        /// Setup for each test
+        /// </summary>
+        [TestInitialize]
+        public void Setup()
         {
 
+
+        }
+
+        [TestMethod]
+        public void RedeemCoupon()
+        {
+            // need to know user redeeming coupon
 
 
         }
