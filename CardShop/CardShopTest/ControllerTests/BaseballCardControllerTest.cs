749e7a6152a019e152489029443b5f4f7fe019d6
roblesdon1710 <roblesdon1017@hotmail.com>
2013-03-28 13:25:17 -0400

    Created Test class for the BaseballCardController class.

---
+++ CardShop/CardShopTest/ControllerTests/BaseballCardControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CardShop.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CardShopTest.ControllerTests
+{
+    [TestClass]
+    public class BaseballCardControllerTest
+    {
+        private BaseballCardController Controller { get; set; }
+
+        public BaseballCardControllerTest()
+        { 
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
