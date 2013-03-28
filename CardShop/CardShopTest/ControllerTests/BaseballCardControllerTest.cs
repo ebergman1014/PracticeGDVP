11904a6d3cefbf16d76b1bcc1f32721507af852c
roblesdon1710 <roblesdon1017@hotmail.com>
2013-03-28 14:15:15 -0400

    Continued working on the test for the BaseballCardContrller class.

--- CardShop/CardShopTest/ControllerTests/BaseballCardControllerTest.cs
+++ CardShop/CardShopTest/ControllerTests/BaseballCardControllerTest.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using Moq;
 using CardShop.Controllers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using System.Web;
+using System.Web.Routing;
+using CardShop;
 
 namespace CardShopTest.ControllerTests
 {
@@ -8,14 +13,41 @@ namespace CardShopTest.ControllerTests
     public class BaseballCardControllerTest
     {
         private BaseballCardController Controller { get; set; }
+        private RouteCollection Routes { get; set; }
+        private Mock<HttpResponseBase> Response { get; set; }
+        private Mock<HttpRequestBase> Request { get; set; }
+        private Mock<HttpContextBase> Context { get; set; }
+        private Mock<ControllerContext> ControllerContext { get; set; }
 
         public BaseballCardControllerTest()
-        { 
+        {
+            Routes = new RouteCollection();
+            RouteConfig.RegisterRoutes(Routes);
+
+            Request = new Mock<HttpRequestBase>(MockBehavior.Strict);
+            Request.SetupGet(x => x.ApplicationPath).Returns("/");
+            Request.SetupGet(x => x.Url).Returns(new Uri("http://localhost/a", UriKind.Absolute));
+            Request.SetupGet(x => x.ServerVariables).Returns(new System.Collections.Specialized.NameValueCollection());
+
+            Response = new Mock<HttpResponseBase>(MockBehavior.Strict);
+
+            Context = new Mock<HttpContextBase>(MockBehavior.Strict);
+            Context.SetupGet(x => x.Request).Returns(Request.Object);
+            Context.SetupGet(x => x.Response).Returns(Response.Object);
+
+            Controller = new BaseballCardController();
+            Controller.ControllerContext = new ControllerContext(Context.Object, new RouteData(), Controller);
+            Controller.Url = new UrlHelper(new RequestContext(Context.Object, new RouteData()), Routes);
         }
 
         [TestMethod]
-        public void TestMethod1()
+        public void IndexRedirect()
         {
+            string viewName = "/BaseballCard/Upload";
+            var result = Controller.Index() as ViewResult;
+            Assert.IsNotNull(result);
+
+            Assert.AreEqual(viewName, result.ViewData["ReturnUrl"]);
         }
     }
 }
